95944fb5b2e46c40564535a629e06bffb0df4f93
AlexeyKalamkarov <kalamkarovalexey@gmail.com>
2025-03-28 16:00:40 +0300

    init

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 "use strict"
-console.log('')
+
+let carsMap = new Map(
+    [
+        ['volvo', 'XC60'],
+        ['toyota', 'camry'],
+        ['porsche', 'panamera'],
+    ]
+)
+// for (const [key, value] of carsMap) {
+//     console.log(key);
+//     console.log(value);
+// }
+// console.log([...carsMap]);
+// console.log(carsMap);
+  carsMap =  new Map([...carsMap].map(el => el.reverse()));
+  console.log(carsMap)
+  
+  let numTest = new Map([
+    [1, 6],
+    [2, 16],
+    [3, 256],
+    [4, 1024],
+  ])
+  for ( const [key, value] of numTest) {
+    console.log(key, Math.sqrt(value))
+  }
